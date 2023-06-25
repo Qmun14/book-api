8d4f7b46b84a4b2b258279e5042ad23f6d27cf93
Qmun14 <mamunramdhan@gmail.com>
2023-06-25 20:08:38 +0700

    4. Menambahkan filter-book.dto.ts dan Implementasi validasion untuk getBooks

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -13,6 +13,7 @@ import {
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { FilterBookDto } from './dto/filter-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -25,12 +26,9 @@ export class BooksController {
   constructor(private booksService: BooksService) { }
 
   @Get()
-  getBooks(
-    @Query('title') title: string,
-    @Query('author') author: string,
-    @Query('category') category: string,
-  ) {
-    return this.booksService.getBooks(title, author, category);
+  @UsePipes(ValidationPipe)
+  getBooks(@Query() filter: FilterBookDto) {
+    return this.booksService.getBooks(filter);
   }
 
   @Get('/:id')
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,24 +2,21 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { FilterBookDto } from './dto/filter-book.dto';
 
 @Injectable()
 export class BooksService {
   private books: any[] = [];
 
-  getBooks(title: string, author: string, category: string): any[] {
+  getBooks(filter: FilterBookDto): any[] {
+    const { title, author, category, min_year, max_year } = filter;
     const books = this.books.filter((book) => {
-      let isMatch = true;
-      if (title && book.title != title) {
-        isMatch = false;
-      }
-      if (author && book.author != author) {
-        isMatch = false;
-      }
-      if (category && book.category != category) {
-        isMatch = false;
-      }
-      return isMatch;
+      if (title && book.title != title) return false;
+      if (author && book.author != author) return false;
+      if (category && book.category != category) return false;
+      if (min_year && book.year < min_year) return false;
+      if (max_year && book.year > max_year) return false;
+      return true;
     });
     return books;
   }
